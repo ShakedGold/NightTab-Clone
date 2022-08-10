27a7d7de6acc927e43c17c22553b089d4e3868de
ShakedGold <shakedgoldy@gmail.com>
2022-08-10 19:27:46 +0300

    added search

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-function test() {
-    let grid = document.getElementsByClassName("image-gallery").style.display = "none";
+let grid_visible = false;
+let search_term = "";
+
+function hide() {
+    let grid = document.getElementById("gallery");
+    grid.style.visibility = grid_visible ? "visible" : "hidden";
+    grid_visible = !grid_visible;
+}
+
+function search() {
+    search_term = document.getElementById("search").value;
+    window.location.href = `https://www.google.com/search?q=${search_term.replace(' ', "+")}`;
 }
